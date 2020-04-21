e5833bf5843e3dbe0e4715eb196299e8f6c9b7d4
Jacob <jayjayb100@msn.com>
2020-04-21 12:35:31 -0500

    first go api is working

--- main.go
+++ main.go
@@ -1,22 +1,41 @@
 package main
 
 import (
-"fmt"
-"log"
-"net/http"
-
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
 )
 
-func homePage(w http.ResponseWriter, r *http.Request){
-fmt.Fprintf(w, "Homepage Endpoint hit")
+
+type BlogPost struct {
+	Title string `json:"Title"`
+	Desc string `json:"desc"`
+	Content string `json:"content`
 }
 
-func handleRequests(){
-http.HandleFunc("/", homePage)
-log.Fatal(http.ListenAndServe(":8081", nil))
+type Posts []BlogPost
+
+func allPosts(w http.ResponseWriter, r *http.Request){
+	posts := Posts{
+		BlogPost{Title:"Test",Desc:"Test",Content:"Hello World"},
+	}
+
+	fmt.Println("endpoint hit: All posts endpoint")
+	json.NewEncoder(w).Encode(posts)
+}
+
+func homePage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Homepage Endpoint hit")
+}
+
+func handleRequests() {
+	http.HandleFunc("/", homePage)
+	http.HandleFunc("/allposts",allPosts)
+	log.Fatal(http.ListenAndServe(":8081", nil))
 
 }
 
-func main(){
-handleRequests()
+func main() {
+	handleRequests()
 }
